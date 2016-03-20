842203dcd0f4f0803437de4351eb7a2283382001
Lance Matysik <lancematysik@gmail.com>
2016-03-19 21:47:02 -0500

    initial. libgdx generated project.

---
+++ desktop/src/edu/cvtc/capstone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.cvtc.capstone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.cvtc.capstone.RollRolePlayingGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RollRolePlayingGame(), config);
+	}
+}
