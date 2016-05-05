5cb356bc898d0afb059172c893caf539273aebf1
Lance Matysik <lancematysik@gmail.com>
2016-05-04 21:35:13 -0500

    unused import cleanups

--- core/src/edu/cvtc/capstone/screens/WinGameScreen.java
+++ core/src/edu/cvtc/capstone/screens/WinGameScreen.java
@@ -4,11 +4,8 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -61,7 +58,7 @@ public class WinGameScreen implements Screen{
 
 	    label.setX(400);
 		label.setY(200);
-		label.setWidth(500);;
+		label.setWidth(500);
 	    label.setHeight(200);
 	    label.setFontScale(2);
 	    
